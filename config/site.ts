4bcfa167638bf12dd83c760c7c392e0784c6a3e6
Satyajit Ghana <satyajitghana7@gmail.com>
2024-01-06 12:05:51 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,14 +3,14 @@ export const siteConfig = {
   name: "The School Of AI - Chat",
   url: "https://thesatyajit.com",
   ogImage: "https://thesatyajit.com/og.jpg",
-  description: "Chat with Vision Capabilities!",
+  description: "Chat!",
   links: {
     twitter: "https://twitter.com/thesudoer_",
-    github: "https://github.com/satyajitghana/tsai-chat-vision",
+    github: "https://github.com/satyajitghana/next-openai-llama-chat",
   },
   // modelBackend: "https://rnhzc-49-207-249-85.a.free.pinggy.online/v1",
   // modelBackend: "https://api.theschoolof.ai/v1",
-  modelBackend: "http://54.201.206.59:8080/v1"
+  modelBackend: "http://13.200.17.32:8080/v1"
 };
 
 export type SiteConfig = typeof siteConfig;
