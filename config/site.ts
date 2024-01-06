02c8a0f3fd0e5e4d6f5098470b5233112e632962
Satyajit Ghana <satyajitghana7@gmail.com>
2024-01-06 12:06:28 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -9,8 +9,8 @@ export const siteConfig = {
     github: "https://github.com/satyajitghana/next-openai-llama-chat",
   },
   // modelBackend: "https://rnhzc-49-207-249-85.a.free.pinggy.online/v1",
-  // modelBackend: "https://api.theschoolof.ai/v1",
-  modelBackend: "http://13.200.17.32:8080/v1"
+  modelBackend: "https://api.theschoolof.ai/v1",
+  // modelBackend: "http://13.200.17.32:8080/v1"
 };
 
 export type SiteConfig = typeof siteConfig;
